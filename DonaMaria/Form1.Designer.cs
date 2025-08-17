6a837e834ee0b0f460b9dd5ae30d2eb268cc3afb
Luiz Felipe <lufelipinh26@gmail.com>
2025-08-17 19:39:04 -0300

    17/08/2025, 19:39

--- DonaMaria/Form1.Designer.cs
+++ DonaMaria/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            dataGridView1 = new DataGridView();
+            btnSalvar = new Button();
+            Código = new DataGridViewTextBoxColumn();
+            Nome = new DataGridViewTextBoxColumn();
+            Descrição = new DataGridViewTextBoxColumn();
+            btnAlterar = new DataGridViewButtonColumn();
+            btnExcluir = new DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Código";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(100, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 61);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Descrição";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(75, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(100, 35);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(698, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 79);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ScrollBars = ScrollBars.Vertical;
+            textBox3.Size = new Size(786, 150);
+            textBox3.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Código, Nome, Descrição, btnAlterar, btnExcluir });
+            dataGridView1.Location = new Point(1, 318);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(800, 132);
+            dataGridView1.TabIndex = 6;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Location = new Point(12, 235);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(76, 28);
+            btnSalvar.TabIndex = 7;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // Código
+            // 
+            Código.HeaderText = "Código";
+            Código.Name = "Código";
+            // 
+            // Nome
+            // 
+            Nome.HeaderText = "Nome";
+            Nome.Name = "Nome";
+            Nome.Width = 175;
+            // 
+            // Descrição
+            // 
+            Descrição.HeaderText = "Descrição";
+            Descrição.Name = "Descrição";
+            Descrição.Width = 275;
+            // 
+            // btnAlterar
+            // 
+            btnAlterar.HeaderText = "Alterar";
+            btnAlterar.Name = "btnAlterar";
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.HeaderText = "Excluir";
+            btnExcluir.Name = "btnExcluir";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalvar);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Cadastrar Tipo de Cozinha";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private DataGridView dataGridView1;
+        private Button btnSalvar;
+        private DataGridViewTextBoxColumn Código;
+        private DataGridViewTextBoxColumn Nome;
+        private DataGridViewTextBoxColumn Descrição;
+        private DataGridViewButtonColumn btnAlterar;
+        private DataGridViewButtonColumn btnExcluir;
     }
 }
--- DonaMaria/Form1.cs
+++ DonaMaria/Form1.cs
@@ -6,5 +6,10 @@ namespace DonaMaria
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
