637929175ea2ae09ae850bd91dc1e3d058c93fce
Luiz Felipe <lufelipinh26@gmail.com>
2025-08-17 22:15:18 -0300

    Form "Cadastrar Receita" criado
    dia: 17/08/2025
    hora: 22:15

---
+++ DonaMaria/FrmCadastrarReceita.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace DonaMaria
+{
+    partial class FrmCadastrarReceita
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            dataGridView2 = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn3 = new DataGridViewTextBoxColumn();
+            btnAdicionarIngredientes = new Button();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(102, 27);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(681, 23);
+            textBox2.TabIndex = 20;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 27);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(66, 23);
+            textBox1.TabIndex = 19;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(102, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 15);
+            label2.TabIndex = 17;
+            label2.Text = "Nome da Receita";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 16;
+            label1.Text = "Código";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 83);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(771, 23);
+            textBox3.TabIndex = 23;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 15);
+            label3.TabIndex = 24;
+            label3.Text = "Tipo de Cozinha";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 118);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 15);
+            label4.TabIndex = 25;
+            label4.Text = "Ingredientes";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2, dataGridViewTextBoxColumn3 });
+            dataGridView2.Location = new Point(12, 136);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(596, 119);
+            dataGridView2.TabIndex = 26;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.HeaderText = "Ingrediente";
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            dataGridViewTextBoxColumn2.HeaderText = "Quantidade";
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            dataGridViewTextBoxColumn2.Width = 175;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            dataGridViewTextBoxColumn3.HeaderText = "Unidade de Medida";
+            dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            dataGridViewTextBoxColumn3.Width = 275;
+            // 
+            // btnAdicionarIngredientes
+            // 
+            btnAdicionarIngredientes.Location = new Point(625, 170);
+            btnAdicionarIngredientes.Name = "btnAdicionarIngredientes";
+            btnAdicionarIngredientes.Size = new Size(141, 48);
+            btnAdicionarIngredientes.TabIndex = 27;
+            btnAdicionarIngredientes.Text = "Adicionar Ingredientes";
+            btnAdicionarIngredientes.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 269);
+            label5.Name = "label5";
+            label5.Size = new Size(99, 15);
+            label5.TabIndex = 28;
+            label5.Text = "Modo de Preparo";
+            // 
+            // FrmCadastrarReceita
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(btnAdicionarIngredientes);
+            Controls.Add(dataGridView2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FrmCadastrarReceita";
+            Text = "FrmCadastrarReceita";
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label4;
+        private DataGridView dataGridView2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private Button btnAdicionarIngredientes;
+        private Label label5;
+    }
+}
---
+++ DonaMaria/FrmCadastrarReceita.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DonaMaria
+{
+    public partial class FrmCadastrarReceita : Form
+    {
+        public FrmCadastrarReceita()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
